af937d846d50b7cd1df5f38fb60ce2647cacfd5f
Muhammed Erdinç <erdincmuhammed@gmail.com>
2023-04-01 13:00:17 +0300

    feat: add layout

--- src/App.vue
+++ src/App.vue
@@ -3,5 +3,18 @@ import { RouterView } from 'vue-router'
 </script>
 
 <template>
-  <RouterView />
+  <v-app id="inspire">
+    <v-navigation-drawer>
+      <!--  -->
+    </v-navigation-drawer>
+
+    <v-app-bar>
+      <v-app-bar-nav-icon></v-app-bar-nav-icon>
+      <v-toolbar-title>Application</v-toolbar-title>
+    </v-app-bar>
+
+    <v-main>
+      <RouterView />
+    </v-main>
+  </v-app>
 </template>
