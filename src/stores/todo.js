91d3865a27568a6d6db646bdc9f3abe93e35af9b
Muhammed Erdinç <erdincmuhammed@gmail.com>
2023-04-03 01:26:58 +0300

    feat: add updateItem

--- src/stores/todo.js
+++ src/stores/todo.js
@@ -4,9 +4,17 @@ import { ref } from "vue";
 import { TodoStatus } from '@/constants/index.js';
 
 export const useTodoStore = defineStore("todo", () => {
+  // State
   const items = ref([
     { id: 0, title: "Learn Vue 3", endDate: "2023-09-09", state: TodoStatus.TODO },
+    { id: 1, title: "Learn Vue 2", endDate: "2023-09-10", state: TodoStatus.COMPLETED },
   ]);
 
-  return { items };
+  // Actions
+  const updateItem = (id, item) => {
+    const index = items.value.findIndex((item) => item.id === id);
+    items.value[index] = item;
+  };
+
+  return { items, updateItem };
 });
