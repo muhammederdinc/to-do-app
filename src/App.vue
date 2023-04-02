785d9502d294ac7f021be76c309b69c99fb840eb
Muhammed Erdinç <erdincmuhammed@gmail.com>
2023-04-02 16:49:55 +0300

    feat: add TDHeader

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,8 @@
 <script setup>
 import { RouterView } from 'vue-router'
 import { ref } from 'vue'
+// Components
+import TDHeader from './components/TDHeader.vue'
 
 const isDrawerVisible = ref(false)
 const routingButtons = [
@@ -30,42 +32,11 @@ const routingButtons = [
         />
       </v-list>
     </v-navigation-drawer>
-    
-    <v-app-bar class="px-2" color="#42A5F5">
-      <v-app-bar-nav-icon
-        class="d-sm-none pa-0 ma-0"
-        color="white"
-        @click="isDrawerVisible = !isDrawerVisible"
-      />
 
-      <div class="d-none d-sm-flex">
-        <v-btn
-          v-for="(btn, index) in routingButtons"
-          :key="index"
-          color="white"
-          variant="text"
-          :to="btn.path"
-        >
-          {{ btn.text }}
-        </v-btn>
-      </div>
-
-      <v-spacer />
-
-      <v-btn class="d-none d-sm-flex" color="white" variant="outlined">
-        <v-icon class="mr-1">mdi-plus</v-icon>
-
-        Add To-Do
-      </v-btn>
-
-      <v-btn
-        class="d-sm-none"
-        size="small"
-        variant="outlined"
-        color="white"
-        icon="mdi-plus"
-      />
-    </v-app-bar>
+    <TDHeader
+      :routingButtons="routingButtons"
+      @toggleDrawerVisibility="isDrawerVisible = !isDrawerVisible"
+    />
 
     <v-main>
       <RouterView />
