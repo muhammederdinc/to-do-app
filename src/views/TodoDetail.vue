d5f461c83a0c0cc6ceb2c7f21f609eb031245100
Muhammed Erdinç <erdincmuhammed@gmail.com>
2023-04-04 02:43:04 +0300

    feat: get todo by id

--- src/views/TodoDetail.vue
+++ src/views/TodoDetail.vue
@@ -1,13 +1,16 @@
 <script setup>
+import { useRouter } from 'vue-router'
+import { useTodoStore } from '@/stores/todo'
 
+const router = useRouter()
+const { getTodoById } = useTodoStore()
+
+const { id } = router.currentRoute.value.params
+const todo = getTodoById(Number(id))
 </script>
 
 <template>
   <div>
-    "/todo/{id}"
+    {{ todo }}
   </div>
 </template>
-
-<style>
-
-</style>
