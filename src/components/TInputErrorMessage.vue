f298efacfe2a1caf68203b075a3b000badf14af6
Muhammed Erdinç <erdincmuhammed@gmail.com>
2023-04-03 19:20:56 +0300

    feat: create TInputErrorMessage

---
+++ src/components/TInputErrorMessage.vue
@@ -0,0 +1,18 @@
+<script setup>
+defineProps({
+  message: {
+    type: String,
+    required: true
+  }
+})
+</script>
+
+<template>
+  <div class="v-input__details pl-3 text-error">
+    <div class="v-messages" role="alert" aria-live="polite">
+      <div class="v-messages__message">
+        {{ message }}
+      </div>
+    </div>
+  </div>
+</template>
