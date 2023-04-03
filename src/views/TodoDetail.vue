58e39e50c40589537906e64cbbe296673703d336
Muhammed Erdinç <erdincmuhammed@gmail.com>
2023-04-04 02:57:23 +0300

    style: TodoDetail design

--- src/views/TodoDetail.vue
+++ src/views/TodoDetail.vue
@@ -10,7 +10,32 @@ const todo = getTodoById(Number(id))
 </script>
 
 <template>
-  <div>
-    {{ todo }}
+  <div class="d-flex justify-center pa-4">
+    <v-card
+      class="text-white"
+      color="blue-lighten-2"
+      theme="dark"
+      width="800"
+    >
+      <v-card-title class="text-h5">
+        {{ todo.title }}
+      </v-card-title>
+
+      <v-card-subtitle class="d-flex flex-column">
+        <span>
+          End Date: {{ todo.endDate }}
+        </span>
+
+        <span class="mt-2">
+          Status: {{ todo.state }}
+        </span>
+        </v-card-subtitle>
+
+      <v-card-actions class="d-flex justify-end">
+        <v-btn variant="outlined">
+          Update Todo
+        </v-btn>
+      </v-card-actions>
+    </v-card>
   </div>
 </template>
