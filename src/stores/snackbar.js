afa29e7a90a412da99af11d70f8e206b46c9cd2c
Muhammed Erdinç <erdincmuhammed@gmail.com>
2023-04-04 06:45:33 +0300

    feat: create useSnackbar

---
+++ src/stores/snackbar.js
@@ -0,0 +1,22 @@
+import { defineStore } from "pinia";
+import { reactive } from "vue";
+
+export const useSnackbar = defineStore("snackbar", () => {
+  // State
+  const snackbar = reactive({
+    isVisible: true,
+    message: "İşlem başarılı",
+    color: "success",
+    timeout: 5000,
+  });
+
+  // Actions
+  const showSnackbar = (message = "İşlem başarılı", color = "success", timeout = 5000) => {
+    snackbar.isVisible = true;
+    snackbar.message = message;
+    snackbar.color = color;
+    snackbar.timeout = timeout;
+  };
+
+  return { snackbar, showSnackbar };
+});
