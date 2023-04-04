8f50b91b95fe8f15bee322da01fcd658e7e9623d
Muhammed Erdinç <erdincmuhammed@gmail.com>
2023-04-04 17:10:58 +0300

    feat: add v-card list for mobile view

--- src/views/TodoList/ToDoList.vue
+++ src/views/TodoList/ToDoList.vue
@@ -33,87 +33,154 @@ const updateTask = () => {
 
 <template>
   <div class="t-to-do-list">
-    <v-data-table
-      v-model:sort-by="sortBy"
-      :headers="ToDoListTableHeaders"
-      :items="todoStore.items"
-      :search="search"
-      class="elevation-1"
-    >
-      <template v-slot:item.statusAction="{ item }">
-        <v-checkbox
-          v-model="item.raw.state"
+    <div class="d-none d-sm-flex">
+      <v-data-table
+        v-model:sort-by="sortBy"
+        :headers="ToDoListTableHeaders"
+        :items="todoStore.items"
+        :search="search"
+        class="elevation-1"
+      >
+        <template v-slot:item.statusAction="{ item }">
+          <v-checkbox
+            v-model="item.raw.state"
+            color="success"
+            :value="TodoStatus.COMPLETED"
+            :false-value="TodoStatus.TODO"
+            hide-details
+          />
+        </template>
+  
+        <template v-slot:item.title="{ item }">
+          <router-link :to="{ name: 'todoDetail', params: { id: item.raw.id }}">
+            <span v-if="item.raw.state === TodoStatus.COMPLETED" class="text-decoration-line-through">
+              {{ item.title }}
+            </span>
+            
+            <span v-else>
+              {{ item.title }}
+            </span>
+          </router-link>
+        </template>
+  
+        <template v-slot:item.endDate="{ item }">
+          <span v-if="item.raw.state === TodoStatus.COMPLETED" class="text-decoration-line-through">
+            {{ new Intl.DateTimeFormat("en-US").format(new Date(item.raw.endDate)) }}
+          </span>
+  
+          <span v-else>
+            {{ new Intl.DateTimeFormat("en-US").format(new Date(item.raw.endDate)) }}
+          </span>
+        </template>
+  
+        <template v-slot:top>
+          <div class="t-to-do-list__table-header d-flex align-center pl-3">
+            <h3>My To-do List </h3>
+  
+            <v-spacer />
+  
+            <v-responsive
+              max-width="344"
+            >
+              <v-text-field
+                v-model="search"
+                variant="outlined"
+                label="Search"
+                class="pa-4"
+                clearable
+                density="compact"
+              />
+            </v-responsive>
+          </div>
+        </template>
+  
+        <template v-slot:item.actions="{ item }">
+          <div class="d-flex">
+            <v-btn
+              icon="mdi-pencil"
+              variant="outlined"
+              size="x-small"
+              color="blue"
+              class="mr-2"
+              @click="openGlobalNavigationDrawer(item.raw)"
+            />
+    
+            <v-btn
+              icon="mdi-delete"
+              variant="outlined"
+              size="x-small"
+              color="red"
+              @click="todoStore.deleteTodo(item.raw.id)"
+            />
+          </div>
+        </template>
+      </v-data-table>
+    </div>
+
+    <v-card class="mt-5 d-sm-none" v-for="todo in todoStore.items" :key="todo.id">
+      <v-card-title>
+        {{ todo.title }}
+      </v-card-title>
+
+      <v-card-text class="d-flex flex-column">
+        <span>
+          <v-icon class="mr-1" color="blue">
+            mdi-clock
+          </v-icon>
+
+          <span :class="todo.state === TodoStatus.COMPLETED ? 'text-decoration-line-through' : ''">
+            {{ new Intl.DateTimeFormat("en-US").format(new Date(todo.endDate)) }}
+          </span>
+
+        </span>
+
+        <span class="mt-2">
+          <v-icon class="mr-1" color="blue">
+            {{ todo.state === TodoStatus.COMPLETED ? 'mdi-check' : 'mdi-progress-clock' }}
+          </v-icon>
+
+          {{ todo.state }}
+        </span>
+      </v-card-text>
+
+      <v-card-actions>
+        <v-switch
+          v-model="todo.state"
           color="success"
           :value="TodoStatus.COMPLETED"
           :false-value="TodoStatus.TODO"
           hide-details
         />
-      </template>
+        <v-spacer />
 
-      <template v-slot:item.title="{ item }">
-        <router-link :to="{ name: 'todoDetail', params: { id: item.raw.id }}">
-          <span v-if="item.raw.state === TodoStatus.COMPLETED" class="text-decoration-line-through">
-            {{ item.title }}
-          </span>
-          
-          <span v-else>
-            {{ item.title }}
-          </span>
-        </router-link>
-      </template>
+        <v-btn
+          variant="outlined"
+          size="small"
+          color="blue"
+          class="mr-2"
+          @click="openGlobalNavigationDrawer(todo)"
+        >
+          <v-icon>
+            mdi-pencil
+          </v-icon>
 
-      <template v-slot:item.endDate="{ item }">
-        <span v-if="item.raw.state === TodoStatus.COMPLETED" class="text-decoration-line-through">
-          {{ new Intl.DateTimeFormat("en-US").format(new Date(item.raw.endDate)) }}
-        </span>
+          Edit
+        </v-btn>
 
-        <span v-else>
-          {{ new Intl.DateTimeFormat("en-US").format(new Date(item.raw.endDate)) }}
-        </span>
-      </template>
+        <v-btn
+          variant="outlined"
+          size="small"
+          color="red"
+          @click="todoStore.deleteTodo(todo.id)"
+        >
+          <v-icon>
+            mdi-delete
+          </v-icon>
 
-      <template v-slot:top>
-        <div class="t-to-do-list__table-header d-flex align-center pl-3">
-          <h3>My To-do List </h3>
-
-          <v-spacer />
-
-          <v-responsive
-            max-width="344"
-          >
-            <v-text-field
-              v-model="search"
-              variant="outlined"
-              label="Search"
-              class="pa-4"
-              clearable
-              density="compact"
-            />
-          </v-responsive>
-        </div>
-      </template>
-
-      <template v-slot:item.actions="{ item }">
-        <div class="d-flex">
-          <v-btn
-            icon="mdi-pencil"
-            variant="outlined"
-            size="x-small"
-            color="blue"
-            class="mr-2"
-            @click="openGlobalNavigationDrawer(item.raw)"
-          />
-  
-          <v-btn
-            icon="mdi-delete"
-            variant="outlined"
-            size="x-small"
-            color="red"
-            @click="todoStore.deleteTodo(item.raw.id)"
-          />
-        </div>
-      </template>
-    </v-data-table>
+          Delete
+        </v-btn>
+      </v-card-actions>
+    </v-card>
 
     <Teleport to="#global-navigation-drawer">
       <div class="text-center mt-5">
