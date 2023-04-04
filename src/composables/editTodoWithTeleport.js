c9a59e849556bc751344fb9fbb91a8bb569649a2
Muhammed Erdinç <erdincmuhammed@gmail.com>
2023-04-04 06:55:40 +0300

    feat: add useSnackbarStore in useEditTodoWithTeleport

--- src/composables/editTodoWithTeleport.js
+++ src/composables/editTodoWithTeleport.js
@@ -1,15 +1,18 @@
 import { useGlobalNavigationDrawer } from '@/stores/globalNavigationDrawer'
 import { useTodoStore } from '@/stores/todo'
+import { useSnackbarStore } from '@/stores/snackbar'
 
 export function useEditTodoWithTeleport() {
   // Data
   const { open, close } = useGlobalNavigationDrawer()
   const todoStore = useTodoStore()
+  const { showSnackbar } = useSnackbarStore()
 
   // Methods
   const updateTask = (params) => {
     todoStore.updateTodo(params.id, { ...params })
     close()
+    showSnackbar('To-do updated successfully', 'success')
   }
 
   return {
