dd4a2b39ca22afa17707cb38d976e0583851134b
Muhammed Erdinç <erdincmuhammed@gmail.com>
2023-04-04 13:23:19 +0300

    feat: add ToDoListTableHeaders in constants

--- src/constants/index.js
+++ src/constants/index.js
@@ -15,3 +15,12 @@ export const MenuItems = Object.freeze([
     path: '/to-do-list'
   }
 ]);
+
+export const ToDoListTableHeaders = Object.freeze([
+  { title: '', key: 'statusAction', sortable: false },
+  { title: 'ID', key: 'id', sortable: false },
+  { title: 'Title', key: 'title', sortable: false },
+  { title: 'End Date', key: 'endDate' },
+  { title: 'Status', key: 'state' },
+  { title: 'Actions', key: 'actions', sortable: false },
+]);
