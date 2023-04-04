e32396e0dde4bf0bee6b1bd33e9892eed5a57b27
Muhammed Erdinç <erdincmuhammed@gmail.com>
2023-04-04 05:36:29 +0300

    feat: add computed

--- src/views/TodoDetail/composables/todo.js
+++ src/views/TodoDetail/composables/todo.js
@@ -1,13 +1,15 @@
 import { useRouter } from 'vue-router'
 import { useTodoStore } from '@/stores/todo'
+import { computed } from 'vue'
 
 export function useTodo() {
   // Data
   const router = useRouter()
   const { getTodoById } = useTodoStore()
-
   const { id } = router.currentRoute.value.params
-  const todo = getTodoById(Number(id))
+
+  // Computed
+  const todo = computed(() => getTodoById(Number(id)))
 
   return { todo };
 }
