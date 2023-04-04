11f145aa75cdf7e1d347134a52f32b71557947af
Muhammed Erdinç <erdincmuhammed@gmail.com>
2023-04-04 14:18:11 +0300

    feat: move useSnackbarStore to todo store

--- src/composables/createTodoWithModal.js
+++ src/composables/createTodoWithModal.js
@@ -1,18 +1,15 @@
 import { ref } from 'vue'
 import { useTodoStore } from '@/stores/todo'
-import { useSnackbarStore } from '@/stores/snackbar'
 
 export function useCreateTodoWithModal() {
   // Data
   const isVisible = ref(false)
   const todoStore = useTodoStore()
-  const { showSnackbar } = useSnackbarStore()
 
   // Methods
   const createTodo = (params) => {
     todoStore.addTodo(params)
     isVisible.value = false
-    showSnackbar('To-do has been created successfully', 'success')
   }
 
   return {
