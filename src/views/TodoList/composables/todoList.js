e2d152aa6a76927515cb95bf35bf138d9642e17f
Muhammed Erdinç <erdincmuhammed@gmail.com>
2023-04-04 22:01:28 +0300

    feat: create todoList composable function

--- src/views/TodoList/composables/todoList.js
+++ src/views/TodoList/composables/todoList.js
@@ -1,15 +1,60 @@
-import { ref } from 'vue'
+import { ref, reactive, watch } from 'vue'
+import { useTodoStore } from '@/stores/todo'
 import { TodoStatus, ToDoListTableHeaders } from '@/constants/index.js';
+import { useEditTodoWithTeleport } from '@/composables/editTodoWithTeleport'
 
 export function useTodoList() {
   // Data
   const search = ref('')
   const sortBy = ref([])
+  const filterStatus = ref('')
+  const todoStore = useTodoStore()
+  const todoList = ref(todoStore.items)
+  const navigationFormData = reactive({})
+  const { updateTask, openNavigationDrawer, closeNavigationDrawer } = useEditTodoWithTeleport()
+
+  // Methods
+  const openGlobalNavigationDrawer = (item) => {
+    navigationFormData.id = item.id
+    navigationFormData.title = item.title
+    navigationFormData.endDate = item.endDate
+    navigationFormData.state = item.state
+  
+    openNavigationDrawer()
+  }
+
+  const searchTodo = ({ target }) => {
+    todoList.value = todoStore.items.filter((todo) => todo.title.toLowerCase().includes(target.value.toLowerCase()))
+  }
+
+  const filterTodo = () => {
+    if(!filterStatus.value) {
+      todoList.value = todoStore.items
+  
+      return
+    }
+  
+    todoList.value = todoStore.items.filter((todo) => todo.state === filterStatus.value)
+  }
+
+  // Watchers
+  watch(() => todoStore.items, (newVal) => {
+    todoList.value = newVal
+  },{ deep: true })
 
   return {
     search,
     sortBy,
+    todoList,
+    updateTask,
     TodoStatus,
-    ToDoListTableHeaders
+    searchTodo,
+    filterTodo,
+    filterStatus,
+    navigationFormData,
+    ToDoListTableHeaders,
+    closeNavigationDrawer,
+    openGlobalNavigationDrawer,
+    deleteTodo: todoStore.deleteTodo,
   };
 }
