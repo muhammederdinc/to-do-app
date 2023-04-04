ac0dc3fe54ece73050c0d29469d8001301000d3e
Muhammed Erdinç <erdincmuhammed@gmail.com>
2023-04-04 13:17:18 +0300

    feat: add MenuItems in constants

--- src/constants/index.js
+++ src/constants/index.js
@@ -2,3 +2,16 @@ export const TodoStatus = Object.freeze({
   TODO: 'todo',
   COMPLETED: 'completed'
 });
+
+export const MenuItems = Object.freeze([
+  {
+    text: 'Home Page',
+    value: 'home',
+    path: '/'
+  },
+  {
+    text: 'To-Do List',
+    value: 'list',
+    path: '/to-do-list'
+  }
+]);
