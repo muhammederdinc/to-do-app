11ad78c1bb87991c899ad2e710753117b4d86e0f
Muhammed Erdinç <erdincmuhammed@gmail.com>
2023-04-05 00:56:15 +0300

    feat: create createTodoModal composable function

---
+++ src/components/TCreateTodoModal/composables/createTodoModal.js
@@ -0,0 +1,40 @@
+import { reactive, ref } from 'vue'
+
+export function useCreateTodoModal(emit) {
+  // Data
+  const showModal = true
+  const form = ref(null)
+  const isEndDateErrorMessageVisible = ref(false)
+  const formData = reactive({
+    title: '',
+    endDate: ''
+  })
+
+  const titleRules = [
+    v => !!v || 'Title is required',
+    v => (v && v.length <= 30) || 'Title must be less than 30 characters',
+    v => /^[a-zA-Z ]+$/.test(v) || 'Title can only contain letters and spaces'
+  ]
+
+  // Methods
+  const submit = async () => {
+    isEndDateErrorMessageVisible.value = false
+    const { valid } = await form.value.validate()
+  
+    if (!formData.endDate) {
+      isEndDateErrorMessageVisible.value = true
+    }
+  
+    if (valid && !isEndDateErrorMessageVisible.value) {
+      emit('submit', formData)
+    }
+  }
+  return {
+    form,
+    submit,
+    formData,
+    showModal,
+    titleRules,
+    isEndDateErrorMessageVisible
+  }
+}
