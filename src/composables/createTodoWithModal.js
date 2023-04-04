8925799053c1e77c6412a835d6e74f3916294336
Muhammed Erdinç <erdincmuhammed@gmail.com>
2023-04-04 13:20:48 +0300

    feat: add snackbar

--- src/composables/createTodoWithModal.js
+++ src/composables/createTodoWithModal.js
@@ -1,15 +1,18 @@
-import { useTodoStore } from '@/stores/todo'
 import { ref } from 'vue'
+import { useTodoStore } from '@/stores/todo'
+import { useSnackbarStore } from '@/stores/snackbar'
 
 export function useCreateTodoWithModal() {
   // Data
   const isVisible = ref(false)
   const todoStore = useTodoStore()
+  const { showSnackbar } = useSnackbarStore()
 
   // Methods
   const createTodo = (params) => {
     todoStore.addTodo(params)
     isVisible.value = false
+    showSnackbar('To-do has been created successfully', 'success')
   }
 
   return {
